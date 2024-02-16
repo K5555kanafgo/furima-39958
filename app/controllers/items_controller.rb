4fcf0d6671765704bae3b8b18fd0d899ef1728a3
K5555kanafgo <jinjingshouta09@gmail.com>
2024-02-16 22:13:42 +0900

    Rubocopの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  validates :birthday,         presence: true
+  validates :nickname,           presence: true
+  validates :birthday,           presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
-
-  with_options presence: true, format:  { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } do
+  with_options presence: true,
+               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
     validates :family_name
   end
-  
-  with_options presence: true, format:  { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "is invalid. Input full-width katakana characters" } do
+
+  with_options presence: true,
+               format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :read_first
     validates :read_family
   end
-
 end
