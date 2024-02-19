6a0cc8cc366bdc3d465ecd4360589b7637c6d0bb
K5555kanafgo <jinjingshouta09@gmail.com>
2024-02-19 21:18:16 +0900

    ActiveHash導入及び各バリデーションの設定(ユーザ管理機能及びその他機能実装済み)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,19 @@ class Item < ApplicationRecord
     has_many               :comments
     belongs_to             :user
     has_one_attached       :image
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to             :category
+    belongs_to             :situation
+    belongs_to             :load
+    belongs_to             :prefecture
+    belongs_to             :number_of_day
+
+    validates :title, :explanation, presence: true
+
+    validates :category_id,  numericality:      { other_than: 1 }
+    validates :situation_id,  numericality:     { other_than: 1 }
+    validates :load_id,  numericality:          { other_than: 1 }
+    validates :prefecture_id,  numericality:    { other_than: 1 }
+    validates :number_of_day_id,  numericality: { other_than: 1 }
 end
