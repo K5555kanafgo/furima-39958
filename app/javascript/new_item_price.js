cdfc6251fdbb0a53c3dfb9128e27fdc5c01c18dc
K5555kanafgo <jinjingshouta09@gmail.com>
2024-02-25 22:18:16 +0900

    JavaScript（JSファイル作成）導入

---
+++ app/javascript/new_item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom  = document.getElementById("add-tax-price");
+    const tax = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = tax;
+
+    const profitDom  = document.getElementById("profit");
+    const profit = Math.floor(inputValue - tax);
+    profitDom.innerHTML = profit;
+  });
+});
