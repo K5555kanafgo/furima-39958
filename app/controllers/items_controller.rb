e81f0d6517f8549a494952c9494e67ef71789eb0
K5555kanafgo <jinjingshouta09@gmail.com>
2024-03-02 21:41:28 +0900

    URL直接入力の為、コードを作成した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :item_url, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -39,5 +38,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :explanation, :category_id, :situation_id,
                                  :load_id, :prefecture_id, :number_of_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_url
+    unless current_user == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
