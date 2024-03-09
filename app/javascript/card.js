3785dfc49b827bd3abf552eb394b179521c3b718
K5555kanafgo <jinjingshouta09@gmail.com>
2024-03-09 18:27:02 +0900

    ブラウザのカード情報を取得する2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,15 @@
 const pay = () => {
+
+  const payjp = Payjp('******************************')
+  const elements = payjp.elements();
+  const numberElements = elements.create("cardNumber");
+  const expiryElements = elements.create("cardExpiry");
+  const cvcElemnts = elements.create("cardCvc");
+
+  numberElements.mount("#number-form");
+  expiryElements.mount("#expiry-form");
+  cvcElemnts.mount("#cvc-form");
+
   const form = document.getElementById("charge-form")
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
