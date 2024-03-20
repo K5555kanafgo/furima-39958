b922719ba436c656e1f2cc1763561a5c76fcf86e
K5555kanafgo <jinjingshouta09@gmail.com>
2024-03-20 18:08:23 +0900

    コード修正1

--- app/javascript/new_item_price.js
+++ app/javascript/new_item_price.js
@@ -1,15 +1,18 @@
-window.addEventListener('turbo:load', () => {
-  const priceInput = document.getElementById("item-price");
-  const addTaxDom  = document.getElementById("add-tax-price");
-  const profitDom = document.getElementById("profit");
+function price() {
 
+  const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
     const tax = Math.floor(inputValue * 0.1);
+    const addTaxDom  = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = tax.toLocaleString();
 
+    const profitDom = document.getElementById("profit");
     const profit = Math.floor(inputValue - tax);
     profitDom.innerHTML = profit.toLocaleString();
   });
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
